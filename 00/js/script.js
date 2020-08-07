2c981bad0d80b6f9fb2bf5a5bce4e2c06cb44599
SERGO <sergei181989@gmail.com>
2020-08-07 20:52:38 +0300

    Learning js course today finish.

--- 00/js/script.js
+++ 00/js/script.js
@@ -1,2 +1,34 @@
 'use strict';
 
+if (4 == 9) {
+	console.log('ok!');
+} else {
+	console.log('Error');
+}
+
+const num = 50;
+
+// if (num < 49) {
+// 	console.log('Error');
+// } else if (num > 100) {
+// 	console.log('Много');
+// } else {
+// 	console.log('Верно!');
+// }
+//
+// (num === 50) ? console.log('ok!') : console.log('Error');
+
+switch (num) {
+	case 49:
+		console.log('Error');
+		break;
+	case 100:
+		console.log('Error');
+		break;
+	case 50:
+		console.log('ok!');
+		break;
+	default:
+		console.log('Default!');
+		break;
+}
